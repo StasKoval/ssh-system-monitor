073c5f106853097e795f90afb067b8f58ee4815e
Michael Ford <mtford@gmail.com>
2014-03-06 18:43:15 +0000

    Cleaned config.

--- config.js
+++ config.js
@@ -2,16 +2,6 @@
  * Created by mtford on 21/02/2014.
  */
 
-//exports.servers = [{
-//    name: 'MosaycDev',
-//    host: '46.51.201.85',
-//    port: 22,
-//    username: 'ubuntu',
-//    privateKey: '/Users/mtford/Dropbox/Drake/Server-Side/dev.pem',
-//    monitoringOptions: {
-//        diskSpace: ['/home/ubuntu/', '/mnt']
-//    }
-//}];
 exports.servers = [
     {
     name: 'Clarity',
@@ -23,30 +13,9 @@ exports.servers = [
         diskSpace: ['/home/']
     }
 }
-//    {
-//        name: 'MosaycDev',
-//        host: '46.51.201.85',
-//        port: 22,
-//        username: 'ubuntu',
-//        privateKey: '/Users/mtford/Dropbox/Drake/Server-Side/dev.pem',
-//        monitoringOptions: {
-//            diskSpace: ['/home/ubuntu/', '/mnt']
-//        }
-//    },
-//    {
-//        name: 'MosaycProd',
-////        host: '54.228.223.187',
-//        host: '1.228.223.187',
-//        port: 22,
-//        username: 'ubuntu',
-//        privateKey: '/Users/mtford/Dropbox/Drake/Server-Side/mosayc.pem',
-//        monitoringOptions: {
-//            diskSpace: ['/home/ubuntu/', '/mnt']
-//        }
-//    }
 ];
 exports.dataFile = '/tmp/ssh-system-monitor/ssm.dat';
 exports.rate = 1000;
 exports.poolSize = 10;
 exports.maintainConnections = 2;
-exports.logLevel = 'info';
+exports.logLevel = 'trace';
