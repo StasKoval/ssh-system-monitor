cd3d87ad9993f5c58bf1a3bdb81244c28f46b6e5
Michael Ford <mtford@gmail.com>
2014-03-15 17:30:14 +0000

    Vagrant for integration testing.

--- config.js
+++ config.js
@@ -1,19 +1,31 @@
-/**
- * Created by mtford on 21/02/2014.
- */
+/*
+   This config file hooks up to the vagrant virtual machines specified in
+   the Vagrantfile. Useful for integration testing.
+*/
 
-exports.servers = [
-    {
-    name: 'Clarity',
-    host: '188.226.141.90',
-    port: 22,
-    username: 'clarity',
-    privateKey: '/Users/mtford/.ssh/id_rsa',
-    monitoringOptions: {
-        diskSpace: ['/home/']
-    }
-}
-];
+var exec = require('exec-sync');
+
+identityFileConfig = 'vagrant ssh-config precise64 | grep IdentityFile'
+privateKey = exec(identityFileConfig).split(' ').slice(-1).pop()
+username = 'vagrant'
+
+exports.servers = [{
+    name: 'precise64',
+    host: '192.168.50.2',
+    username: username,
+    privateKey: privateKey
+},{
+    name: 'debian64',
+    host: '192.168.50.3',
+    username: username,
+    privateKey: privateKey
+},{
+    name: 'centos64',
+    host: '192.168.50.5',
+    username: username,
+    privateKey: privateKey
+}];
+
 exports.dataFile = '/tmp/ssh-system-monitor/ssm.dat';
 exports.rate = 1000;
 exports.poolSize = 10;
