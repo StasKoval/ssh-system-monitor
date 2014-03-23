784bf41a144c6f09349785c90cf17785cf5f6939
Michael Ford <mtford@gmail.com>
2014-03-23 12:05:14 +0000

    Expanded integration tests with further analysis.

--- tests/integration/test.js
+++ tests/integration/test.js
@@ -6,7 +6,50 @@ var expect = require("chai").expect
     , system = require('../../src/system')
     , config = require('../../src/config')
     , logger = config.logger
-    , Nedb =  require('nedb');
+    , Nedb =  require('nedb')
+    , _ = require('underscore');
+
+function analyseDocuments(docs) {
+    var i, doc;
+    var analysis = {
+        occur:{}
+    };
+    for (i=0; i<docs.length; i++) {
+        doc = docs[i];
+        if (!(doc.host in analysis.occur)) {
+            analysis.occur[doc.host] = 1;
+        }
+        else {
+            analysis.occur[doc.host]++;
+        }
+    }
+    var hosts = _.pluck(config.servers, 'host');
+    var total = _.reduce(Object.keys(analysis.occur), function(memo, key) {
+        return memo + analysis.occur[key]
+    }, 0);
+    var occurances = _.map(hosts, function(host) {
+        var occur = analysis.occur[host];
+        return occur ? occur : 0;
+    });
+    var percentages = _.map(occurances, function(x) {
+        return (100 * (x/total)).toFixed(2).toString() + '%';
+    });
+    process.stdout.write('\n==================\n');
+    process.stdout.write(  '  Begin Analysis  \n');
+    process.stdout.write(  '==================\n\n');
+    process.stdout.write(  'Data points: \n');
+    _.each(_.zip(hosts, occurances), function(tuple) {
+        process.stdout.write('  * ' + tuple[0] + ' => ' + tuple[1] + '\n');
+    });
+    process.stdout.write('\n');
+    process.stdout.write(  'Data point ratio: \n');
+    _.each(_.zip(hosts, percentages), function(tuple) {
+        process.stdout.write('  * ' + tuple[0] + ' => ' + tuple[1] + '\n');
+    });
+    process.stdout.write('\n==================\n');
+    process.stdout.write(  '   End Analysis   \n');
+    process.stdout.write(  '==================\n\n');
+}
 
 describe('Integration Tests', function () {
     /*
@@ -15,6 +58,8 @@ describe('Integration Tests', function () {
 
     var db = new Nedb();
     var testLength = 5000;
+
+    this.timeout(testLength * 2);
 
     before(function () {
         system.start(config, db);
@@ -29,6 +74,17 @@ describe('Integration Tests', function () {
             });
             done();
         }, testLength);
+    });
+
+    after(function (done) {
+        db.find({}, function(err, docs) {
+            if (docs.length) {
+                analyseDocuments(docs);
+            }
+            system.terminate(function () {
+                done();
+            });
+        });
     });
 
 });
